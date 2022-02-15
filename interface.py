8560880643f23b638e40377de61b9e3a722ad93a
Anurag Goyal <anuraggoyal.awr@gmail.com>
2022-02-15 18:50:01 +0530

    [Add]: api caller script
    - Reads input from console
    - Constructs request body and prints output on console

---
+++ interface.py
@@ -0,0 +1,37 @@
+import requests
+import sys
+
+
+def exec_remote(apiUrl)-> str:
+    if len(sys.argv) < 2:
+        print("mention question number")
+        return
+
+    question = sys.argv[1]
+    args = [' '.join(sys.argv[2:])]
+
+    while True:
+        line = input()
+        if not line:
+            break
+        else:
+            args.append(line)
+
+    req_body = {
+        "question": question,
+        "args": args
+    }
+
+    r = requests.get(apiUrl, json=req_body)
+    return r.text
+
+
+def main():
+    apiUrl = "https://urtc.example.com"
+    response = exec_remote(apiUrl)
+    print(response)
+
+
+if __name__ == "__main__":
+    main()
+
