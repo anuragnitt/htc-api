11ae81603ff4f130f030fc6f47105d113e034aad
Anurag Goyal <anuraggoyal.awr@gmail.com>
2022-02-17 20:21:53 +0530

    [Feat:] multiple source languages for compilation

--- assets/compile.sh
+++ assets/compile.sh
@@ -4,19 +4,20 @@
 SCRIPT_DIR=`cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd`
 SRC_DIR="$SCRIPT_DIR/src"
 BIN_DIR="$SCRIPT_DIR/binary"
-
-SRC_EXT=".cpp"
 OUT_EXT=".out"
 
 
-for file in `ls -a $BIN_DIR | grep $OUT_EXT`
-do
+for file in `ls -a $BIN_DIR | grep $OUT_EXT`; do
     rm $file 2>/dev/null
 done
 
 
-for file in `ls -a $SRC_DIR | grep $SRC_EXT`
-do
+for file in `ls -a $SRC_DIR`; do
+    SRC_EXT=".${file##*.}"
     outfile=$BIN_DIR/`basename $file $SRC_EXT`$OUT_EXT
-    g++ -static "$SRC_DIR/$file" -o $outfile
+
+    [[ "$SRC_EXT" == ".c" ]] && \
+        gcc -static "$SRC_DIR/$file" -o $outfile
+    [[ "$SRC_EXT" == ".cpp" ]] || [[ "$SRC_EXT" == ".c++" ]] && \
+        g++ -static "$SRC_DIR/$file" -o $outfile
 done
