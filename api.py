8dce859c95debd8b520d18824cfc6170f9c859d2
Anurag Goyal <anuraggoyal.awr@gmail.com>
2022-02-19 17:33:52 +0000

    [Feat: Uptime check route]

--- api.py
+++ api.py
@@ -6,11 +6,12 @@ from os.path import dirname, realpath, join
 from subprocess import Popen, PIPE
 from datetime import datetime
 from pytz import timezone as make_tz
-from werkzeug.middleware.proxy_fix import ProxyFix
+from werkzeug.exceptions import HTTPException
 import click, json, os, sys, traceback
+from typing import List, Tuple
 
 
-def get_log_msg(info, tb_msg, tz):
+def log_this_event(info: str, tb_msg: str, tz: str, log_file: str)-> None:
     tz = make_tz(tz)
     curr_time = datetime.now(tz)
     curr_time = curr_time.strftime(
@@ -22,16 +23,16 @@ def get_log_msg(info, tb_msg, tz):
 {info}\nEXCEPTION:\n{tb_msg}\
 =======================================================\n\n"
 
-    return log_msg
+    open(log_file, "a").write(log_msg)
 
 
-def set_executable_mode(bin_file: str):
+def set_executable_mode(bin_file: str)-> None:
     mode = os.stat(bin_file).st_mode
     mode |= (mode & 0o444) >> 2
     os.chmod(bin_file, mode)
 
 
-def exec_bin(bin_file: str, args: list)-> str:
+def exec_bin(bin_file: str, args: List[str])-> str:
     set_executable_mode(bin_file)
     argstring = "\n".join(args).encode("utf-8")
 
@@ -45,28 +46,82 @@ def exec_bin(bin_file: str, args: list)-> str:
     return output.decode("utf-8")
 
 
-app = Flask("HTC-API")
+htc_app = Flask("HTC-API")
+
+limiter = Limiter(
+    app=htc_app,
+    key_func=lambda: request.headers.get(
+        "X-Forwarded-For",
+        "127.0.0.1"
+    ),
+    default_limits=["20/minute"]
+)
 
 
-@app.errorhandler(429)
-def ratelimit_handler(e):
-    max_rate = app.config["max_rate"]
-    return f"Only {max_rate} requests are allowed per minute", 429
+@htc_app.errorhandler(404)
+def invalid_route_handler(e)-> Tuple[str, int]:
+    code = e.get_response().status_code
+    return f"The route {request.path} doesn't exist", code
 
 
-@app.route("/", methods=["GET"])
-def api_handler()-> [str, int]:
+@htc_app.errorhandler(429)
+def ratelimit_handler(e)-> Tuple[str, int]:
+    code = e.get_response().status_code
+    max_rate = htc_app.config["max_rate"]
+    return f"Maximum allowed execution rate is {max_rate}", code
+
+
+@htc_app.errorhandler(HTTPException)
+def http_exception_handler(e)-> Tuple[str, int]:
+    tz = htc_app.config["timezone"]
+    log_file = htc_app.config["log_file"]
+
+    info = "This exception was caught in the HTTPException handler"
+    tb_msg = traceback.format_exc()
+    log_this_event(info, tb_msg, tz, log_file)
+
+    code = e.get_response().status_code
+    return f"Oops! A {code} HTTP error occured", code 
+
+
+@htc_app.errorhandler(Exception)
+def server_error_handler(e)-> Tuple[str, int]:
+    tz = htc_app.config["timezone"]
+    log_file = htc_app.config["log_file"]
+
+    info = "This exception was caught in the general Exception handler"
+    tb_msg = traceback.format_exc()
+    log_this_event(info, tb_msg, tz, log_file)
+
+    return "A server side error has occured", 500
+
+
+@htc_app.route("/uptime", methods=["GET"])
+@limiter.exempt
+def uptime_check()-> Tuple[str, int]:
+    return "Don't dig around too much XD", 200
+
+
+@htc_app.route("/", methods=["GET"])
+@limiter.limit(
+    limit_value=lambda: limiter.app.config.get(
+        "max_rate",
+        "20/minute"
+    ),
+    override_defaults=True
+)
+def api_handler()-> Tuple[str, int]:
 
     try:
         if request.method == "GET":
 
-            config_file = app.config["config_file"]
+            config_file = htc_app.config["config_file"]
             config = json.load(open(config_file, "r"))
             execmap = config["execmap"]
 
             params = request.get_json()
             bin_file = join(
-                app.config["bin_dir"],
+                htc_app.config["bin_dir"],
                 execmap[params["question"]]
             )
 
@@ -78,20 +133,19 @@ def api_handler()-> [str, int]:
             return output, 200
 
         else:
-            return "method not implemented", 501
+            return f"{request.method} method is not implemented", 501
 
     except Exception as e:
-        log_file = app.config["log_file"]
-        tz = app.config["timezone"]
+        tz = htc_app.config["timezone"]
+        log_file = htc_app.config["log_file"]
 
-        info = f"REQUEST PARAMETERS:\n\
+        info = f"This event was caught in the execution route handler.\n\
+        REQUEST PARAMETERS:\n\
         {json.dumps(params, indent=4)}"
         tb_msg = traceback.format_exc()
+        log_this_event(info, tb_msg, tz, log_file)
 
-        log_msg = get_log_msg(info, tb_msg, tz)
-        open(log_file, "a").write(log_msg)
-
-        return "invalid parameters", 400
+        return "Invalid parameters", 422
 
 
 @click.command()
@@ -124,21 +178,14 @@ def api_handler()-> [str, int]:
 )
 @click.option(
     "--max-rate",
-    default=20,
-    help="maximum requests allowed per minute per user"
+    default="20/minute",
+    help="maximum requests allowed per IP address"
 )
-def main(bin_dir, config, log_file, timezone, host, port, max_rate):
-
+def main(bin_dir: str, config: str, log_file: str, timezone: str, host: str, port: str or int, max_rate: str)-> None:
     try:
-        app.wsgi_app = ProxyFix(app.wsgi_app, x_for=True)
+        global htc_app
 
-        limiter = Limiter(
-            key_func=get_remote_address,
-            default_limits=[f"{max_rate}/minute"]
-        )
-        limiter.init_app(app)
-
-        app.config.update(
+        htc_app.config.update(
             bin_dir=bin_dir,
             config_file=config,
             log_file=log_file,
@@ -146,20 +193,19 @@ def main(bin_dir, config, log_file, timezone, host, port, max_rate):
             max_rate=max_rate
         )
 
-        app.run(
+        htc_app.run(
             host=host,
             port=port,
             use_reloader=True
         )
     
     except Exception:
-        info = "This exception occured of the route handler"
+        info = "This exception was caught in main()"
         tb_msg = traceback.format_exc()
+        log_this_event(info, tb_msg, timezone, log_file)
 
-        log_msg = get_log_msg(info, tb_msg, timezone)
-        open(log_file, "a").write(log_msg)
+        return
 
 
 if __name__ == "__main__":
     main()
-
